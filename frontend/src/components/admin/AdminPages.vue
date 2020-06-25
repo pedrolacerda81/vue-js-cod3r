49f45eb0f8d6989fc5ec04638234620ca47375e0
Pedro - GitPro <peidom81@gmail.com>
2020-06-25 13:02:44 -0400

    Implements home.vue and adminPages

---
+++ frontend/src/components/admin/AdminPages.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="admin-pages">
+    <PageTitle icon="fa fa-home" main="Administração do Sistema" sub="Cadastros & Cia" />
+  </div>
+</template>
+
+<script>
+import PageTitle from "@/components/template/PageTitle";
+
+export default {
+  name: "AdminPages",
+  components: { PageTitle }
+};
+</script>
+
+<style>
+</style>
